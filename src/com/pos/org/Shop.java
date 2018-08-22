55d347ccd9cf1e3eaa6e09429f14d051c77721ae
ogo <ogookafor@gmail>
2018-08-22 17:08:04 +0300

    Remove duplicate customer.java file

---
+++ src/com/pos/org/Shop.java
@@ -0,0 +1,25 @@
+package com.pos.org;
+
+public class Shop {
+	private String name;
+	private String location;
+	//private ArrayList<Sellers> sellers;  code this when its ready
+	
+	public void open() {}
+	public void close() {}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	
+	
+}
