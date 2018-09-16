e4cd77cdac1721fd77cac2e889cd9c8fea202977
ogo <ogookafor@gmail>
2018-09-16 16:16:51 +0300

    - Ability to create,read,update and delete product categories - delete previous classes in inventory pkg - Included changes to databas objects

---
+++ src/com/pos/inventory/ProductCategory.java
@@ -0,0 +1,28 @@
+package com.pos.inventory;
+
+import java.math.BigDecimal;
+
+public class ProductCategory {
+	private BigDecimal id;
+	private String 	   name;
+	private String     desc;
+	public BigDecimal getId() {
+		return id;
+	}
+	public void setId(BigDecimal id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+}
---
+++ src/com/pos/inventory/ProductCategoryDAO.java
@@ -0,0 +1,135 @@
+package com.pos.inventory;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.pos.database.Database;
+
+public class ProductCategoryDAO {
+	
+	public static void createCategory(ProductCategory category) {
+		String sql = "INSERT INTO product_category "
+				   + "values(?,?,?)";
+		
+		try {
+			Connection cxtn =  Database.getDatabaseConnection();
+		    PreparedStatement stmt = cxtn.prepareStatement(sql);
+		    
+		    stmt.setBigDecimal(1, category.getId());
+		    stmt.setString(2,category.getName());
+		    stmt.setString(3,category.getDesc());
+		    stmt.execute();
+		    stmt.close();
+		    cxtn.close();
+		    
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static List<ProductCategory> getAllCategory() {
+	    List<ProductCategory> listOfCategories = new ArrayList<ProductCategory>();
+ 		String sql = "SELECT * FROM product_category ";
+		
+		try {
+			Connection cxtn =  Database.getDatabaseConnection();
+		    ResultSet rows  = cxtn.createStatement().executeQuery(sql);
+		    
+		    while(rows.next()) {
+		    	ProductCategory category = new ProductCategory();
+		    	category.setId(rows.getBigDecimal(1));
+		    	category.setName(rows.getString(2));
+		    	category.setDesc(rows.getString(3));
+		    	listOfCategories.add(category);
+		    }
+		    
+		    listOfCategories.forEach((category)->{
+		    	System.out.println(category.getId());
+		    	System.out.println(category.getName());
+		    	System.out.println(category.getDesc());
+		    });
+		      
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return listOfCategories;
+	}
+	
+	
+	public static ProductCategory getCategory(BigDecimal id) {
+    	ProductCategory category = new ProductCategory();
+ 		String sql = "SELECT * "
+ 				   + "FROM product_category "
+ 				   + "WHERE cat_id = ?";
+		
+		try {
+			Connection cxtn =  Database.getDatabaseConnection();
+		    PreparedStatement stmt  = cxtn.prepareStatement(sql);
+		    stmt.setBigDecimal(1, id);
+		    ResultSet row = stmt.executeQuery();
+		    
+		    if(row.next()) {
+		    	category.setId(row.getBigDecimal(1));
+		    	category.setName(row.getString(2));
+		    	category.setDesc(row.getString(3));
+		    }
+		    
+	    	System.out.println(category.getId());
+	    	System.out.println(category.getName());
+	    	System.out.println(category.getDesc());
+		      
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return category;
+	}
+	
+	
+	public static void updateCategory(ProductCategory category) {
+		String sql = "UPDATE product_category "
+				   + "SET    cat_name = ?, "
+				   + "       cat_desc = ?  "
+				   + "WHERE  cat_id   = ?";
+		
+		try {
+			Connection cxtn =  Database.getDatabaseConnection();
+		    PreparedStatement stmt = cxtn.prepareStatement(sql);
+		    
+		    stmt.setString(1,category.getName());
+		    stmt.setString(2,category.getDesc());
+		    stmt.setBigDecimal(3, category.getId());
+
+		    stmt.executeUpdate();
+		    stmt.close();
+		    cxtn.close();
+		    
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public static void deleteCategory(BigDecimal id) {
+		String sql = "DELETE FROM product_category "
+				   + "WHERE  cat_id = ?";
+		
+		try {
+			Connection cxtn =  Database.getDatabaseConnection();
+		    PreparedStatement stmt = cxtn.prepareStatement(sql);
+		    
+		    stmt.setBigDecimal(1,id);
+		    stmt.execute();
+		    stmt.close();
+		    cxtn.close();
+		    
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
