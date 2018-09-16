cd1a794fbf7d92f8a8f738639f07ed1374d26c00
AndyEssien <essienandy1993@gmail.com>
2018-09-16 02:29:09 +0100

    essiendandy changed some data on branch.java and transaction

--- src/com/pos/org/Branch.java
+++ src/com/pos/org/Branch.java
@@ -1,8 +1,5 @@
 package com.pos.org;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 
 import com.pos.database.Database;
 
@@ -12,46 +9,6 @@ public class Branch {
 	     private String branchAddr;
 	      private String branchState;
 	       private String branchLga;
-	   
-	   
-	   // a method which is called any time a branch needs to be created.
-	   public void createBranch() {
-		    Connection myConnect = null;
-		     PreparedStatement prepareStatement = null;
-		    
-		   try {
-			   myConnect = Database.getDatabaseConnection();
-			     prepareStatement = myConnect.prepareStatement("insert into branch "
-			   	   + "(branchName,branchAddress,branchState,branchLGA)"
-			   		+ " values(?,?,?,?);");
-			          prepareStatement.setString(1,this.branchName);
-			           prepareStatement.setString(2,this.branchAddr);
-			            prepareStatement.setString(3,this.branchState);
-			             prepareStatement.setString(4,this.branchLga);
-			               prepareStatement.execute();
-			                 System.out.println("Branch Succesfully created!");
-			                  prepareStatement.close();
-			                    myConnect.close();}
-		   
-		    catch(Exception error){
-			    error.printStackTrace();}
-		   
-		   // try and catch stops here.	   
-	   }
-
-	   // a method which is called any time a branch need some information from a db.
-	   public void readBranch() {
-		   
-	   }
-	   
-	   public void updateBranch() {
-		   
-	   }
-	   
-	   public void deleteBranch() {
-  
-	   }
-	   
 
 	public int getBranchID() {
 		return branchID;
--- src/com/pos/sales/StartTransaction.java
+++ src/com/pos/sales/StartTransaction.java
@@ -4,13 +4,19 @@ import java.util.Scanner;
 
 import com.pos.account.Attendant;
 import com.pos.org.Branch;
+import com.pos.org.BranchDAO;
 
 public class StartTransaction {
+	
 	public static void main(String args[]) {
 
 		      Branch branch = new Branch();
-		        Scanner input = new Scanner(System.in);
-		          System.out.println("Create new branch. \nPlease fill in the account details below.".toUpperCase()); 
+		        Scanner input,chosedNumber,newName; 
+		        input = new Scanner(System.in);
+		        newName = new Scanner(System.in);
+		         chosedNumber = new Scanner(System.in);
+		    /*            
+		     System.out.println("Create new branch. \nPlease fill in the account details below.".toUpperCase()); 
 		  
 		         System.out.println("Enter the Branch name.");
 		            String brnName = input.nextLine();
@@ -28,7 +34,78 @@ public class StartTransaction {
 		                branch.setBranchAddr(brnAddress);
 		                 branch.setBranchState(brnState);
 		                  branch.setBranchLga(brnLga);
-		                   branch.createBranch();
+		                   branch.createBranch(); 
+		               */       
+		                  
+		        System.out.println(" \t welcome administrator! \n please enter a branch id to make changes .");
 		      
+		             int brnID = input.nextInt();
+		             branch.setBranchID(brnID);
+		              System.out.println("what detail do you want to change ?");
+		               System.out.println("1.Branch Name. \n 2.Branch Addrress.\n 3.Branch State.\n 4.Branch local government.");   
+		                String Name;
+		                  String Addrress;
+		                   String State;
+		                    String LGA;
+		                     int Number = chosedNumber.nextInt();
+		                   
+		                   if(Number == 1) {
+		                  
+		                System.out.println("Please Enter the new Branch Name.");
+		            	  Name = newName.nextLine();
+		            	    BranchDAO.getBranch(branch);
+		            	    branch.setBranchName(Name);
+		            	     BranchDAO.updateBranch(branch);
+		            	      System.out.println("the branch Name for ID "+brnID+" has been changed to "
+		            	       +Name.toUpperCase());
+		            	       input.close();
+		            	       newName.close();
+		            	       chosedNumber.close();
+		            	       
+		            	       
+		               }else if(Number == 2)
+		                 {
+		            	   
+		            	      System.out.println(" please Enter the new Branch Addrress.");
+		            	       Addrress = newName.nextLine();
+		            	       branch.setBranchAddr(Addrress);
+		            	        BranchDAO.updateBranch(branch);
+		            	        System.out.println("the branch Addrress for ID "+brnID+" has been changed to "
+		 		            	       +Addrress.toUpperCase());
+		 		            	       input.close();
+		 		            	       newName.close();
+		 		            	       chosedNumber.close();}
+		                   
+		               else if(Number == 3)
+		               {
+		            	     System.out.println("please Enter a new and current state of this Branch.");
+		            	     State = newName.nextLine();
+		            	     branch.setBranchState(State);
+		            	     BranchDAO.updateBranch(branch);
+		            	     System.out.println("the branch State for ID "+brnID+" has been changed to "
+				            	       +State.toUpperCase());
+				            	       input.close();
+				            	       newName.close();
+				            	       chosedNumber.close();}
+		                   
+		               else if(Number == 4)
+		               {
+		            	    System.out.println("please Enter a new local government Area for this Branch.");
+		            	    LGA = newName.nextLine();
+		            	    branch.setBranchLga(LGA);
+		            	    BranchDAO.updateBranch(branch);
+		            	    System.out.println("the local government Area for Branch ID "+brnID+" has been changed to "
+				            	       +LGA.toUpperCase());
+				            	       input.close();
+				            	       newName.close();
+				            	       chosedNumber.close();}
+		                   
+		               else {
+		            	   
+		            	   System.out.println(" sorry no match was found !".toUpperCase());
+		            	   
+		               }
+		              
+		                   
 	}
 }
